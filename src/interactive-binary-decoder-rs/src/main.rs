d4c70ccbb2ba0ad0f52dcabb7051359909b68771
W. Brian Gourlie <bgourlie@gmail.com>
2018-02-08 00:45:20 -1000

    WIP

--- src/interactive-binary-decoder-rs/src/main.rs
+++ src/interactive-binary-decoder-rs/src/main.rs
@@ -14,9 +14,11 @@ use bitreader::BitReader;
 
 #[derive(Clone, Serialize, Deserialize)]
 struct Float64 {
-    sign: bool,
-    exponent: Vec<bool>,
-    mantissa: Vec<bool>,
+    sign_bit: bool,
+    exponent_bits: Vec<bool>,
+    exponent_value: u16,
+    mantissa_bits: Vec<bool>,
+    mantissa_value: u64,
 }
 
 js_serializable!(Float64);
@@ -24,31 +26,44 @@ js_serializable!(Float64);
 impl Float64 {
     fn new(val: f64) -> Float64 {
         let raw_bytes: [u8; 8] = unsafe { std::mem::transmute(val) };
-        let mut reader = BitReader::new(&raw_bytes);
 
-        let sign = reader.read_bool().unwrap();
+        let (sign_bit, exponent_bits, mantissa_bits) = {
+            let mut reader = BitReader::new(&raw_bytes);
 
-        let mut exponent = Vec::with_capacity(11);
-        for _ in 0..11 {
-            exponent.push(reader.read_bool().unwrap());
-        }
+            let mut mantissa_bits = Vec::with_capacity(52);
+            for _ in 0..52 {
+                mantissa_bits.push(reader.read_bool().unwrap());
+            }
 
-        let mut mantissa = Vec::with_capacity(52);
-        for _ in 0..52 {
-            mantissa.push(reader.read_bool().unwrap());
-        }
+            let mut exponent_bits = Vec::with_capacity(11);
+            for _ in 0..11 {
+                exponent_bits.push(reader.read_bool().unwrap());
+            }
+
+            let sign_bit = reader.read_bool().unwrap();
+            (sign_bit, exponent_bits, mantissa_bits)
+        };
+
+        let (exponent_value, mantissa_value) = {
+            let mut reader = BitReader::new(&raw_bytes);
+            let mantissa_value = reader.read_u64(52).unwrap();
+            let exponent_value = reader.read_u16(11).unwrap();
+            (exponent_value, mantissa_value)
+        };
 
         Float64 {
-            sign,
-            exponent,
-            mantissa,
+            sign_bit,
+            exponent_bits,
+            exponent_value,
+            mantissa_bits,
+            mantissa_value,
         }
     }
 }
 
 fn main() {
     stdweb::initialize();
-    let f64_bits = Float64::new(1.213212);
+    let f64_bits = Float64::new(1.0);
     update(f64_bits);
     stdweb::event_loop();
 }
