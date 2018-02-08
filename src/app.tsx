21c9a723edbc5271a607a82bb2899535f921b7b3
W. Brian Gourlie <bgourlie@gmail.com>
2018-02-07 21:41:11 -1000

    bits!

--- src/app.tsx
+++ src/app.tsx
@@ -2,14 +2,36 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 interface AppWindow extends Window {
-  update: (props: HelloProps) => void;
+    update: (props: FloatValue) => void;
 }
 
-interface HelloProps { compiler: string; framework: string; }
+interface FloatValue {
+    sign: boolean,
+    exponent: [boolean],
+    mantissa: [boolean]
+}
 
-const Hello = (props: HelloProps) => <h1>Hello from {props.compiler} and {props.framework}!</h1>;
+const Float = (props: FloatValue) => {
+    const exponentBits = props.exponent.map((bit, i) => <div key={i}>{bit ? "1" : "0"}</div>);
+    const mantissaBits = props.mantissa.map((bit, i) => <div key={i}>{bit ? "1" : "0"}</div>);
+    return (
+        <div className="float">
+            <div className="sign">
+                <div className="bits">
+                    <div>{props.sign ? "1" : "0"}</div>
+                </div>
+            </div>
+            <div className="exponent">
+                <div className="bits">{exponentBits}</div>
+            </div>
+            <div className="mantissa">
+                <div className="bits">{mantissaBits}</div>
+            </div>
+        </div>
+    );
+};
 
-(window as AppWindow).update = (state: HelloProps) => {
-    ReactDOM.render(Hello(state), document.getElementById("example"));
+(window as AppWindow).update = (state: FloatValue) => {
+    ReactDOM.render(Float(state), document.getElementById("bits"));
 };
 
--- src/interactive-binary-decoder-rs/src/main.rs
+++ src/interactive-binary-decoder-rs/src/main.rs
@@ -7,30 +7,53 @@ extern crate serde_derive;
 #[macro_use]
 extern crate serde_json;
 
+extern crate bitreader;
+
 use stdweb::web::document;
+use bitreader::BitReader;
 
 #[derive(Clone, Serialize, Deserialize)]
-struct HelloProps {
-    compiler: String,
-    framework: String
+struct Float64 {
+    sign: bool,
+    exponent: Vec<bool>,
+    mantissa: Vec<bool>,
 }
 
-js_serializable!(HelloProps);
+js_serializable!(Float64);
+
+impl Float64 {
+    fn new(val: f64) -> Float64 {
+        let raw_bytes: [u8; 8] = unsafe { std::mem::transmute(val) };
+        let mut reader = BitReader::new(&raw_bytes);
+
+        let sign = reader.read_bool().unwrap();
+
+        let mut exponent = Vec::with_capacity(11);
+        for _ in 0..11 {
+            exponent.push(reader.read_bool().unwrap());
+        }
+
+        let mut mantissa = Vec::with_capacity(52);
+        for _ in 0..52 {
+            mantissa.push(reader.read_bool().unwrap());
+        }
+
+        Float64 {
+            sign,
+            exponent,
+            mantissa,
+        }
+    }
+}
 
 fn main() {
     stdweb::initialize();
-
-    let hello_props = HelloProps {
-        compiler: "rust webassembly".to_owned(),
-        framework: "react + webassembly".to_owned()
-    };
-
-    update(hello_props);
+    let f64_bits = Float64::new(1.213212);
+    update(f64_bits);
     stdweb::event_loop();
 }
 
-fn update(props: HelloProps) {
-    let example_div = document().get_element_by_id("example").unwrap();
+fn update(props: Float64) {
     js! {
         update( @{props} );
     }
