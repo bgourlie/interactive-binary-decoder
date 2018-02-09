be61386e82eb740399ac0247c62b4336c4f2ea34
W. Brian Gourlie <bgourlie@gmail.com>
2018-02-09 05:11:58 -0600

    Better bit decoding

--- src/interactive-binary-decoder-rs/src/main.rs
+++ src/interactive-binary-decoder-rs/src/main.rs
@@ -4,12 +4,8 @@ extern crate stdweb;
 #[macro_use]
 extern crate serde_derive;
 
-#[macro_use]
-extern crate serde_json;
-
 use std::f64;
 use std::str::FromStr;
-use std::fmt::Write;
 use stdweb::unstable::TryInto;
 use stdweb::web::{document, IEventTarget};
 use stdweb::web::event::KeyupEvent;
@@ -25,7 +21,7 @@ macro_rules! enclose {
     };
 }
 
-#[derive(Clone, Serialize, Deserialize)]
+#[derive(Serialize, Deserialize)]
 struct Float64 {
     sign_bit: bool,
     exponent_bits: Vec<bool>,
@@ -55,32 +51,17 @@ impl Float64 {
             unsafe { std::mem::transmute(mantissa_bytes) }
         };
 
-        let bit_string: Vec<char> = {
-            let mut raw_bytes = f64_bytes.clone();
-            &raw_bytes.reverse();
+        let sign_bit = to_bit(&f64_bytes, 63);
 
-            let mut s = String::new();
-            for &byte in &raw_bytes {
-                write!(&mut s, "{:0>8b}", byte).expect("Couldn't write byte string to buffer");
-            }
-            s.chars().collect()
-        };
+        let mut exponent_bits = Vec::with_capacity(11);
+        for i in 0..11 {
+            exponent_bits.push(to_bit(&f64_bytes, 62 - i));
+        }
 
-        let (sign_bit, exponent_bits, mantissa_bits) = {
-            let sign_bit = char_to_bit(bit_string[0]);
-
-            let mut exponent_bits = Vec::with_capacity(11);
-            for i in 1..12 {
-                exponent_bits.push(char_to_bit(bit_string[i]));
-            }
-
-            let mut mantissa_bits = Vec::with_capacity(52);
-            for i in 12..64 {
-                mantissa_bits.push(char_to_bit(bit_string[i]));
-            }
-
-            (sign_bit, exponent_bits, mantissa_bits)
-        };
+        let mut mantissa_bits = Vec::with_capacity(52);
+        for i in 0..52 {
+            mantissa_bits.push(to_bit(&f64_bytes, 51 - i));
+        }
 
         Float64 {
             sign_bit,
@@ -93,14 +74,10 @@ impl Float64 {
     }
 }
 
-fn char_to_bit(c: char) -> bool {
-    if c == '0' {
-        false
-    } else if c == '1' {
-        true
-    } else {
-        panic!("Unexpected character {}", c);
-    }
+fn to_bit(f64_bytes: &[u8], bit_position: usize) -> bool {
+    let byte_index = bit_position / 8;
+    let internal_bit_position = bit_position % 8;
+    f64_bytes[byte_index] & (1 << internal_bit_position) > 0
 }
 
 fn main() {
