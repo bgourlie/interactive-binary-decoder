25efa8e06fe3f04b4afb70e400ab5b2d1e394c65
W. Brian Gourlie <bgourlie@gmail.com>
2018-04-06 20:40:23 -0500

    Formatting

--- src/interactive-binary-decoder-rs/Cargo.toml.d.ts
+++ src/interactive-binary-decoder-rs/Cargo.toml.d.ts
@@ -1,14 +1,13 @@
-declare module 'Cargo.toml' {
+declare module "Cargo.toml" {
+  export interface Float {
+    sign_bit: boolean;
+    exponent_bits: [boolean];
+    exponent_value: number;
+    mantissa_bits: [boolean];
+    mantissa_value: number;
+    native_value: number;
+    parsed_from: string;
+  }
 
-    export interface Float {
-        sign_bit: boolean,
-        exponent_bits: [boolean],
-        exponent_value: number,
-        mantissa_bits: [boolean],
-        mantissa_value: number,
-        native_value: number,
-        parsed_from: string
-    }
-
-    export function parse_float(input: string): Float;
+  export function parse_float(input: string): Float;
 }
