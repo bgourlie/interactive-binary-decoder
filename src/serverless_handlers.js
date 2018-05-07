e39c9d7aa540f6a7b5a52b7421ab551fe73c3c85
W. Brian Gourlie <bgourlie@gmail.com>
2018-05-07 01:37:30 -0500

    Getting file from S3

--- src/serverless_handlers.js
+++ src/serverless_handlers.js
@@ -1,2 +1,3 @@
+const AWS = require("aws-sdk");
 require("../serverless_dist/ssr_handler");
-module.exports.ssr = parcelRequire("1").ssr;
+module.exports.ssr = parcelRequire("1").ssr(new AWS.S3());
--- src/ssr_handler.ts
+++ src/ssr_handler.ts
@@ -2,22 +2,38 @@ import * as ReactDOMServer from "react-dom/server";
 import { APIGatewayEvent, Callback, Context, Handler } from "aws-lambda";
 import { rootElement } from "./app";
 import { StyleSheetServer } from "aphrodite/no-important";
+import * as AWS from "aws-sdk";
+import getSettings from "./settings";
 
-export const ssr: Handler = (
-  event: APIGatewayEvent,
-  context?: Context,
-  cb?: Callback
-) => {
-  const body = StyleSheetServer.renderStatic(() =>
-    ReactDOMServer.renderToString(rootElement())
-  );
+export const ssr: (s3: AWS.S3) => Handler = (s3: AWS.S3) => {
+  return (event: APIGatewayEvent, context?: Context, cb?: Callback) => {
+    const settings = getSettings();
+    const params = {
+      Bucket: settings.staticAssetsS3Bucket,
+      Key: "root.html"
+    };
 
-  const response = {
-    statusCode: 200,
-    body: body
+    s3
+      .getObject(params)
+      .promise()
+      .then((data: any) => {
+        console.log(data);
+
+        const body = StyleSheetServer.renderStatic(() =>
+          ReactDOMServer.renderToString(rootElement())
+        );
+
+        const response = {
+          statusCode: 200,
+          body: body
+        };
+
+        if (cb) {
+          cb(null, response);
+        }
+      })
+      .catch((e: any) => {
+        throw e;
+      });
   };
-
-  if (cb) {
-    cb(null, response);
-  }
 };
