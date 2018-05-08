cfe6766411ad6839bd1d0b3128e20bd909f00b9c
W. Brian Gourlie <bgourlie@gmail.com>
2018-05-07 21:46:19 -0500

    SSR getting close

--- src/ssr_handler.ts
+++ src/ssr_handler.ts
@@ -3,8 +3,10 @@ import { APIGatewayEvent, Callback, Context, Handler } from "aws-lambda";
 import { rootElement } from "./app";
 import { StyleSheetServer } from "aphrodite/no-important";
 import * as AWS from "aws-sdk";
+import * as cheerio from "cheerio";
 import getSettings from "./settings";
 
+// We inject the s3 dependency because we don't want to bundle the aws-sdk, we want the version that is supplied by AWS
 export const ssr: (s3: AWS.S3) => Handler = (s3: AWS.S3) => {
   return (event: APIGatewayEvent, context?: Context, cb?: Callback) => {
     const settings = getSettings();
@@ -16,16 +18,26 @@ export const ssr: (s3: AWS.S3) => Handler = (s3: AWS.S3) => {
     s3
       .getObject(params)
       .promise()
-      .then((data: any) => {
-        console.log(data);
+      .then(data => {
+        if (!data.Body) {
+          throw new Error("Empty body returned when requesting root.html");
+        }
+
+        const appMarkup = data.Body.toString();
 
         const body = StyleSheetServer.renderStatic(() =>
           ReactDOMServer.renderToString(rootElement())
         );
 
+        const $ = cheerio.load(appMarkup);
+        $("#app-container").append(body.html);
+        $("style[data-aphrodite]").append(body.css.content);
         const response = {
           statusCode: 200,
-          body: body
+          headers: {
+            "Content-Type": "text/html"
+          },
+          body: $.xml()
         };
 
         if (cb) {
